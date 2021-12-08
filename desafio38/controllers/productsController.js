4585b34e3a74f918b818865fb1390cbc0d95baec
Felipe Lerda <lerdafelipe@gmail.com>
2021-12-08 01:37:54 -0300

    agregado

--- desafio38/controllers/productsController.js
+++ desafio38/controllers/productsController.js
@@ -4,22 +4,27 @@ const productsSchema = require('../schemas/productsSchema');
 const getProducts = async (req, res)=>{
     const products = await productsSchema.find();
 
-    res.json(products);
+    return products;
+    //res.json(products);
 };
 
-const getOneProduct = async (req, res)=>{
-    const {id} = req.params;
+const getOneProduct = async (nombre)=>{
+    //const {id} = req.params;
 
-    const product = await productsSchema.findById(id);
+    const product = await productsSchema.find({nombre: nombre});
 
-    res.json(product);
+    return product;
+    //res.json(product);
 };
 
-const postProduct = async (req, res)=>{
-    const newProduct = new productsSchema(req.body);
-    let productSave = await newProduct.save();
+const postProduct = async (product)=>{
+    //const newProduct = new productsSchema(req.body);
+    const newProuct = new productsSchema(product);
 
-    res.json(productSave);
+    let productSave = await newProuct.save();
+
+    return productSave;
+    //res.json(productSave);
 };
 
 const putProduct = async (req, res)=>{
@@ -48,42 +53,3 @@ module.exports = {
     putProduct, 
     deleteProduct
 }
-
-
-var express = require('express');
-var { graphqlHTTP }  = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-// GraphQL schema
-//https://graphql.org/graphql-js/basic-types/
-var schema = buildSchema(`
-    type Query {
-        products: [Productos],
-        products: [Productos],
-    },
-    type Mutation {
-        postProduct(id: Int!, nombre: String,categoria: String!,stock: Number!, precio: Number!): Product
-    },
-    type Productos {
-        nombre: String,
-        categoria: String,
-        stock: Number,
-        precio: Number
-    }    
-`);
-
-
-
-// Root resolver
-var root = {
-    product: getOneProduct,
-    products: getProducts,
-    postProduct: postProduct
-};
-
-
-router.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-}));
--- desafio38/routes/productos.route.js
+++ desafio38/routes/productos.route.js
@@ -11,14 +11,54 @@ const {
 } = require('../controllers/productsController');
 
 
+
+const { graphqlHTTP }  = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// GraphQL schema
+//https://graphql.org/graphql-js/basic-types/
+var schema = buildSchema(`
+    type Query {
+        product: [Productos],
+        products: [Productos],
+    },
+    type Mutation {
+        postProduct(nombre: String,categoria: String!,stock: Int!, precio: Int!): Productos
+    },
+    type Productos {
+        nombre: String,
+        categoria: String,
+        stock: Int,
+        precio: Int,
+        id: String
+    }    
+`);
+
+
+
+// Root resolver
+var root = {
+    product: getOneProduct,
+    products: getProducts,
+    newproduct: postProduct
+};
+
+
+router.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+
 //Ruta de listar productos
-router.get('/', getProducts);
+//router.get('/', getProducts);
 
 //Ruta de listar un solo producto
-router.get('/:id', getOneProduct);
+//router.get('/:id', getOneProduct);
 
 //Ruta post para guardar un product
-router.post('/', postProduct);
+//router.post('/', postProduct);
 
 //Ruta post para actualizar un product
 router.put('/:id', putProduct);
